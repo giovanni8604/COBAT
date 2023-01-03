0ccb293d4b0cd3929b59da028eb7bb87e51e563f
giovanni.bermudez <giovanni.bermudez@cobatlaxcala.edu.mx>
2023-01-03 16:44:19 -0600

    primer commit

---
+++ assets/js/sweet_alert.js
@@ -0,0 +1,62 @@
+
+Swal.fire({
+	// title:
+	// text: '/assets/archivos/pdf/2022/ficha_de_inscripcion_2022.xlsx',
+	html: 'Descarga el  , ' +
+    '<a href="/COBAT/assets/archivos/pdf/2022/PVMS_Convocatoria_2023-2024_COBACH_TLAXCALA.pdf">pdf</a> ',
+	// icon:
+	// confirmButtonText:
+	// footer:
+	// width:
+	// padding:
+	// background:
+	grow: 'fullscreen',
+	// backdrop:
+	//timer: '50000',
+	//timerProgressBar: 'true',
+	// toast:
+	// position:
+	// allowOutsideClick:
+	// allowEscapeKey:
+	// allowEnterKey:
+	// stopKeydownPropagation:
+
+	// input:
+	// inputPlaceholder:
+	// inputValue:
+	// inputOptions:
+	
+	//  customClass:
+	// 	container:
+	// 	popup:
+	// 	header:
+	// 	title:
+	// 	closeButton:
+	// 	icon:
+	// 	image:
+	// 	content:
+	// 	input:
+	// 	actions:
+	// 	confirmButton:
+	// 	cancelButton:
+	// 	footer:	
+
+	// showConfirmButton:
+	// confirmButtonColor:
+	// confirmButtonAriaLabel:
+
+	// showCancelButton:
+	// cancelButtonText:
+	// cancelButtonColor:
+	// cancelButtonAriaLabel:
+	
+	// buttonsStyling:
+	// showCloseButton:
+	// closeButtonAriaLabel:
+
+
+	imageUrl: '/COBAT/assets/images/slider/usicamm.jpg'
+	// imageWidth:
+	// imageHeight:
+	// imageAlt:
+});
