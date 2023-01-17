013998b7acc40345075b9d16d237ba1567d547ae
giovanni.bermudez <giovanni.bermudez@cobatlaxcala.edu.mx>
2023-01-17 11:04:38 -0600

    cuenta publica 4to trimestre

--- assets/js/sweet_alert.js
+++ assets/js/sweet_alert.js
@@ -1,20 +1,11 @@
 Swal.fire({
-	title: '<strong>TERCER <u>COLOQUIO</u></strong>',
+    html: `<h1>TERCER COLOQUIO</h1>
+    <p>INTERNACIONAL<strong> COBAT</strong></p>
+    <br>
+    <a href="/assets/archivos/pdf/2023/convocatoria_ponencias.pdf">Imprimir PDF</a>
+    `,
 	imageUrl: '/assets/images/img_actividades_cult_depor/3er_coloqio.png',
-	html:
-	  'You can use <b>bold text</b>, ' +
-	  '<a href="//sweetalert2.github.io">links</a> ' +
-	  'and other HTML tags',
-	showCloseButton: true,
-	showCancelButton: true,
-	focusConfirm: false,
-	confirmButtonText:
-	  '<i class="fa fa-thumbs-up"></i> Great!',
-	confirmButtonAriaLabel: 'Thumbs up, great!',
-	cancelButtonText:
-	  '<i class="fa fa-thumbs-down"></i>',
-	cancelButtonAriaLabel: 'Thumbs down'
-  })
+});
 
 // ORIGINAL
 // Swal.fire({
