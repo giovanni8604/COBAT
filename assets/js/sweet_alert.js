776c67ca8370226361cd37bc3777ca49a5f568cf
giovanni.bermudez <giovanni.bermudez@cobatlaxcala.edu.mx>
2023-01-17 09:29:43 -0600

    tercer coloquio SE QUITA

--- assets/js/sweet_alert.js
+++ assets/js/sweet_alert.js
@@ -1,62 +1,80 @@
-
 Swal.fire({
-	// title:
-	//text: '/assets/archivos/pdf/2022/ficha_de_inscripcion_2022.xlsx',
-	html: 'Descarga el, ' +
-    '<a href="/COBAT/assets/archivos/pdf/2023/convocatoria_ponencias.pdf">(PDF)</a> ',
-	// icon:
-	// confirmButtonText:
-	// footer:
-	// width:
-	// padding:
-	// background:
-	grow: 'row',
-	// backdrop:
-	//timer: '50000',
-	//timerProgressBar: 'true',
-	// toast:
-	// position:
-	// allowOutsideClick:
-	// allowEscapeKey:
-	// allowEnterKey:
-	// stopKeydownPropagation:
+	title: '<strong>TERCER <u>COLOQUIO</u></strong>',
+	imageUrl: '/assets/images/img_actividades_cult_depor/3er_coloqio.png',
+	html:
+	  'You can use <b>bold text</b>, ' +
+	  '<a href="//sweetalert2.github.io">links</a> ' +
+	  'and other HTML tags',
+	showCloseButton: true,
+	showCancelButton: true,
+	focusConfirm: false,
+	confirmButtonText:
+	  '<i class="fa fa-thumbs-up"></i> Great!',
+	confirmButtonAriaLabel: 'Thumbs up, great!',
+	cancelButtonText:
+	  '<i class="fa fa-thumbs-down"></i>',
+	cancelButtonAriaLabel: 'Thumbs down'
+  })
 
-	// input:
-	// inputPlaceholder:
-	// inputValue:
-	// inputOptions:
+// ORIGINAL
+// Swal.fire({
+// 	// title:
+// 	//text: '/assets/archivos/pdf/2022/ficha_de_inscripcion_2022.xlsx',
+// 	html: 'Descarga el, ' +
+//     '<a href="/COBAT/assets/archivos/pdf/2023/convocatoria_ponencias.pdf">(PDF)</a> ',
+// 	// icon:
+// 	// confirmButtonText:
+// 	// footer:
+// 	// width:
+// 	// padding:
+// 	// background:
+// 	grow: 'row',
+// 	// backdrop:
+// 	//timer: '50000',
+// 	//timerProgressBar: 'true',
+// 	// toast:
+// 	// position:
+// 	// allowOutsideClick:
+// 	// allowEscapeKey:
+// 	// allowEnterKey:
+// 	// stopKeydownPropagation:
+
+// 	// input:
+// 	// inputPlaceholder:
+// 	// inputValue:
+// 	// inputOptions:
 	
-	//  customClass:
-	// 	container:
-	// 	popup:
-	// 	header:
-	// 	title:
-	// 	closeButton:
-	// 	icon:
-	// 	image:
-	// 	content:
-	// 	input:
-	// 	actions:
-	// 	confirmButton:
-	// 	cancelButton:
-	// 	footer:	
+// 	//  customClass:
+// 	// 	container:
+// 	// 	popup:
+// 	// 	header:
+// 	// 	title:
+// 	// 	closeButton:
+// 	// 	icon:
+// 	// 	image:
+// 	// 	content:
+// 	// 	input:
+// 	// 	actions:
+// 	// 	confirmButton:
+// 	// 	cancelButton:
+// 	// 	footer:	
 
-	// showConfirmButton:
-	// confirmButtonColor:
-	// confirmButtonAriaLabel:
+// 	// showConfirmButton:
+// 	// confirmButtonColor:
+// 	// confirmButtonAriaLabel:
 
-	// showCancelButton:
-	// cancelButtonText:
-	// cancelButtonColor:
-	// cancelButtonAriaLabel:
+// 	// showCancelButton:
+// 	// cancelButtonText:
+// 	// cancelButtonColor:
+// 	// cancelButtonAriaLabel:
 	
-	// buttonsStyling:
-	// showCloseButton:
-	// closeButtonAriaLabel:
+// 	// buttonsStyling:
+// 	// showCloseButton:
+// 	// closeButtonAriaLabel:
 
 
-	imageUrl: 'assets/images/slider/3er_coloqio.png'
-	// imageWidth:
-	// imageHeight:
-	// imageAlt:
-});
+// 	imageUrl: 'assets/images/slider/3er_coloqio.png'
+// 	// imageWidth:
+// 	// imageHeight:
+// 	// imageAlt:
+// });
