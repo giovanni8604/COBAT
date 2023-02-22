fd3eefcc64a64f3687c7549d1163529a0199a789
giovanni.bermudez <giovanni.bermudez@cobatlaxcala.edu.mx>
2023-02-21 20:54:44 -0600

    convocatoria USICAMM

--- assets/js/sweet_alert.js
+++ assets/js/sweet_alert.js
@@ -2,9 +2,9 @@ Swal.fire({
     html: `<h1></h1>
     <p><strong></strong></p>
     <br>
-    <a href="/COBAT/assets/archivos/pdf/2023/libro_Celia_Carro.pdf" target="_blank">Imprimir PDF</a>
+    <a href="/assets/archivos/pdf/2023/TLAX_COBACH_23-24.pdf" target="_blank">Descargar PDF</a>
     `,
-	imageUrl: '/COBAT/assets/images/img_actividades_cult_depor/Banner_Manual_de_Cancer.jpg',
+	imageUrl: '/assets/images/img_actividades_cult_depor/BANNER_CONVOCATORIA_USICAMM_ADMISION_23_24.jpg',
 });
 
 // ORIGINAL
