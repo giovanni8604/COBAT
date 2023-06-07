71b6f93f47081e5e155fc0dac75b44101bf21b0e
giovanni.bermudez <giovanni.bermudez@cobatlaxcala.edu.mx>
2023-06-07 10:43:06 -0600

    sweet alert voleybol

--- assets/js/sweet_alert.js
+++ assets/js/sweet_alert.js
@@ -1,71 +1,72 @@
-Swal.fire({
-    html: `<h1></h1>
-    <p><strong></strong></p>
-    <br>
-    <a href="http://189.204.49.118/sistema.php" target="_blank">Clic Aquí</a>
-    `,
-	imageUrl: '/COBAT/assets/images/img_actividades_cult_depor/Convocatoria Preinscripciones 2023.png',
-});
-//<a href="/COBAT/assets/archivos/pdf/2023/TLAX_COBACH_23-24.pdf" target="_blank">Descargar PDF</a>
-// ORIGINAL
 // Swal.fire({
-// 	// title:
-// 	//text: '/assets/archivos/pdf/2022/ficha_de_inscripcion_2022.xlsx',
-// 	html: 'Descarga el, ' +
-//     '<a href="/COBAT/assets/archivos/pdf/2023/convocatoria_ponencias.pdf">(PDF)</a> ',
-// 	// icon:
-// 	// confirmButtonText:
-// 	// footer:
-// 	// width:
-// 	// padding:
-// 	// background:
-// 	grow: 'row',
-// 	// backdrop:
-// 	//timer: '50000',
-// 	//timerProgressBar: 'true',
-// 	// toast:
-// 	// position:
-// 	// allowOutsideClick:
-// 	// allowEscapeKey:
-// 	// allowEnterKey:
-// 	// stopKeydownPropagation:
-
-// 	// input:
-// 	// inputPlaceholder:
-// 	// inputValue:
-// 	// inputOptions:
-	
-// 	//  customClass:
-// 	// 	container:
-// 	// 	popup:
-// 	// 	header:
-// 	// 	title:
-// 	// 	closeButton:
-// 	// 	icon:
-// 	// 	image:
-// 	// 	content:
-// 	// 	input:
-// 	// 	actions:
-// 	// 	confirmButton:
-// 	// 	cancelButton:
-// 	// 	footer:	
-
-// 	// showConfirmButton:
-// 	// confirmButtonColor:
-// 	// confirmButtonAriaLabel:
-
-// 	// showCancelButton:
-// 	// cancelButtonText:
-// 	// cancelButtonColor:
-// 	// cancelButtonAriaLabel:
-	
-// 	// buttonsStyling:
-// 	// showCloseButton:
-// 	// closeButtonAriaLabel:
-
-
-// 	imageUrl: 'assets/images/slider/3er_coloqio.png'
-// 	// imageWidth:
-// 	// imageHeight:
-// 	// imageAlt:
+//     html: `<h1></h1>
+//     <p><strong></strong></p>
+//     <br>
+//     <a href="http://189.204.49.118/sistema.php" target="_blank">Clic Aquí</a>
+//     `,
+// 	imageUrl: '/COBAT/assets/images/img_actividades_cult_depor/Convocatoria Preinscripciones 2023.png',
 // });
+//<a href="/COBAT/assets/archivos/pdf/2023/TLAX_COBACH_23-24.pdf" target="_blank">Descargar PDF</a>
+
+// ORIGINAL
+Swal.fire({
+	// title:
+	//text: '/assets/archivos/pdf/2022/ficha_de_inscripcion_2022.xlsx',
+	// html: 'Descarga el, ' +
+    // '<a href="/COBAT/assets/archivos/pdf/2023/convocatoria_ponencias.pdf">(PDF)</a> ',
+	// icon:
+	// confirmButtonText:
+	// footer:
+	// width:
+	// padding:
+	// background:
+	grow: 'row',
+	// backdrop:
+	timer: '10000',
+	timerProgressBar: 'true',
+	// toast:
+	// position:
+	// allowOutsideClick:
+	// allowEscapeKey:
+	// allowEnterKey:
+	// stopKeydownPropagation:
+
+	// input:
+	// inputPlaceholder:
+	// inputValue:
+	// inputOptions:
+	
+	//  customClass:
+	// 	container:
+	// 	popup:
+	// 	header:
+	// 	title:
+	// 	closeButton:
+	// 	icon:
+	// 	image:
+	// 	content:
+	// 	input:
+	// 	actions:
+	// 	confirmButton:
+	// 	cancelButton:
+	// 	footer:	
+
+	// showConfirmButton:
+	// confirmButtonColor:
+	// confirmButtonAriaLabel:
+
+	// showCancelButton:
+	// cancelButtonText:
+	// cancelButtonColor:
+	// cancelButtonAriaLabel:
+	
+	// buttonsStyling:
+	// showCloseButton:
+	// closeButtonAriaLabel:
+
+
+	imageUrl: 'assets/images/slider/logo intercolegial de baile.jpg'
+	// imageWidth:
+	// imageHeight:
+	// imageAlt:
+});
