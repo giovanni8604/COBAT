37a2f1ed85b77a22b4fcd8721ec121c82ba9e1fa
giovanni <giovanni.bermudez@cobatlaxcala.edu.mx>
2026-01-15 14:57:47 -0600

    se sube proyecto

---
+++ assets/js/sweet_alert.js
@@ -0,0 +1,71 @@
+// Swal.fire({
+//     html: `<h1></h1>
+//     <p><strong></strong></p>
+//     <br>
+//     <a href="http://189.204.49.118/sistema.php" target="_blank">Clic Aquí</a>
+//     `,
+// 	imageUrl: '/COBAT/assets/images/img_actividades_cult_depor/Convocatoria Preinscripciones 2023.png',
+// });
+//<a href="/COBAT/assets/archivos/pdf/2023/TLAX_COBACH_23-24.pdf" target="_blank">Descargar PDF</a>
+
+// ORIGINAL
+Swal.fire({
+	// title:
+	//text: '/COBAT/assets/images/slider/diseño Ediems.jpg',
+	//html: 'INSCRIBETE',
+	// icon:
+	confirmButtonText: '<a href="http://sise.cobatlaxcala.edu.mx/aspirantes/consultar-turno" target="_blank">AQUÍ</a>',
+	// footer:
+	// width:
+	// padding:
+	// background:
+	grow: 'column',
+	// backdrop:
+	timer: '90000',
+	timerProgressBar: 'true',
+	// toast:
+	// position:
+	// allowOutsideClick: 'false',
+	// allowEscapeKey: 'false',
+	// allowEnterKey: 'false',
+	// stopKeydownPropagation: 'false',
+
+	// input:
+	// inputPlaceholder:
+	// inputValue:
+	// inputOptions:
+	
+	//  customClass:
+	// 	container:
+	// 	popup:
+	// 	header:
+	// 	title:
+	// 	closeButton:
+	// 	icon:
+	// 	image:
+	// 	content:
+	// 	input:
+	// 	actions:
+	// 	confirmButton:
+	// 	cancelButton:
+	// 	footer:	
+
+	// showConfirmButton:
+	// confirmButtonColor:
+	// confirmButtonAriaLabel:
+
+	// showCancelButton:
+	// cancelButtonText:
+	// cancelButtonColor:
+	// cancelButtonAriaLabel:
+	
+	// buttonsStyling:
+	// showCloseButton:
+	// closeButtonAriaLabel:
+
+
+	imageUrl: '/COBAT/assets/images/slider/resultados.jpg'
+	// imageWidth:
+	// imageHeight:
+	// imageAlt:
+});
