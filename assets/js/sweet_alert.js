374ba588ac431b1d55b3fd39935a6c0714e11364
giovanni8604 <giovanni.bermudez@cobatlaxcala.edu.mx>
2026-08-12 11:20:01 -0600

    se sube css, fonts, js

---
+++ assets/js/sweet_alert.js
@@ -0,0 +1,73 @@
+Swal.fire({
+  title: '¡Haz clic en la imagen!',
+  html: `
+    <a href="https://sise.cobatlaxcala.edu.mx/sistema.php" style="display: block;">
+      <img src="assets/images/slider/registro de aspirantes.png" >
+    </a>
+  `,
+  timer: '10000',
+  showConfirmButton: false // Ocultamos el botón principal si solo quieres el clic en la imagen
+});
+
+//style="width: 100%; max-width: 1136px; border-radius: 8px;"
+// ORIGINAL
+	//Swal.fire({
+	// title:
+	//   text: '/COBAT/assets/images/slider/diseño Ediems.jpg',
+	//	html: 'INSCRIBETE',
+	// icon:
+	//	confirmButtonText: '<a href="assets/archivos/pdf/2026/convocatoria_ingreso_2026-2027.jpg" target="_blank">AQUÍ</a>',
+	// footer:
+	// width:
+	// padding:
+	// background:
+	 //grow: 'fullscreen',
+	// backdrop:
+	   //timer: '90000',
+	   //timerProgressBar: 'true',
+	// toast:
+	// position:
+	// allowOutsideClick: 'false',
+	// allowEscapeKey: 'false',
+	// allowEnterKey: 'false',
+	// stopKeydownPropagation: 'false',
+
+	// input:
+	// inputPlaceholder:
+	// inputValue:
+	// inputOptions:
+	
+	//  customClass:
+	// 	container:
+	// 	popup:
+	// 	header:
+	// 	title:
+	// 	closeButton:
+	// 	icon:
+	// 	image:
+	// 	content:
+	// 	input:
+	// 	actions:
+	// 	confirmButton:
+	// 	cancelButton:
+	// 	footer:	
+
+	// showConfirmButton:
+	// confirmButtonColor:
+	// confirmButtonAriaLabel:
+
+	// showCancelButton:
+	// cancelButtonText:
+	// cancelButtonColor:
+	// cancelButtonAriaLabel:
+	
+	// buttonsStyling:
+	// showCloseButton:
+	// closeButtonAriaLabel:
+
+
+	 //imageUrl: 'assets/images/slider/registro de aspirantes.png'
+	// imageWidth:
+	// imageHeight:
+	// imageAlt:
+//});
